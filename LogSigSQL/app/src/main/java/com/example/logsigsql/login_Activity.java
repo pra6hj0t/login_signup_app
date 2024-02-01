439c31f146b86941247a3481d22e8e0f99b5c9e9
pra6hj0t <157087592+pra6hj0t@users.noreply.github.com>
2024-01-31 17:25:31 -0700

    data

---
+++ LogSigSQL/app/src/main/java/com/example/logsigsql/login_Activity.java
@@ -0,0 +1,51 @@
+package com.example.logsigsql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.logsigsql.databinding.ActivityLoginBinding;
+
+public class login_Activity extends AppCompatActivity {
+    ActivityLoginBinding binding;
+    DatabaseHelper databaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        databaseHelper = new DatabaseHelper(this);
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = binding.loginEmail.getText().toString();
+                String password = binding.loginPassword.getText().toString();
+                if(email.equals("")||password.equals(""))
+                    Toast.makeText(login_Activity.this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkCredentials = databaseHelper.checkEmailPassword(email, password);
+                    if(checkCredentials == true){
+                        Toast.makeText(login_Activity.this, "Login Successfully!", Toast.LENGTH_SHORT).show();
+                        Intent intent  = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(login_Activity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        binding.signupRequired.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login_Activity.this, signupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
